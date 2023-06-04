99f048c553cd29cf8c39e31f31462b6c4ab8c0a9
Henrique Rocha <carloshrochar7@gmail.com>
2023-06-04 15:54:17 -0300

    Laboratorio 2

---
+++ Laboratorio_GUI/src/view/Label.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+
+public class Label extends JLabel {
+	private static final long serialVersionUID = 6382943506800819142L;
+
+	public Label (String texto) {
+		this.setText(texto);
+		
+	}
+}
